198eff78096857f3d2f174ae20b687558e85e201
Gan <geekwithgun@gmail.com>
2018-10-22 19:33:47 +0800

    setup with fonts and navigation

--- shoplet/android/app/src/main/java/com/shoplet/MainApplication.java
+++ shoplet/android/app/src/main/java/com/shoplet/MainApplication.java
@@ -3,6 +3,8 @@ package com.shoplet;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+
+import com.reactnativenavigation.NavigationApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -11,35 +13,66 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+import com.oblador.vectoricons.VectorIconsPackage;
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+// public class MainApplication extends Application implements ReactApplication {
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
-    }
+//   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//     @Override
+//     public boolean getUseDeveloperSupport() {
+//       return BuildConfig.DEBUG;
+//     }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+//     @Override
+//     protected List<ReactPackage> getPackages() {
+//       return Arrays.<ReactPackage>asList(
+//           new MainReactPackage()
+//       );
+//     }
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+//     @Override
+//     protected String getJSMainModuleName() {
+//       return "index";
+//     }
+//   };
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     SoLoader.init(this, /* native exopackage */ false);
+//   }
+// }
+
+
+ public class MainApplication extends NavigationApplication {
+
+     @Override
+     public boolean isDebug() {
+         // Make sure you are using BuildConfig from your own application
+         return BuildConfig.DEBUG;
+     }
+
+     protected List<ReactPackage> getPackages() {
+         // Add additional packages you require here
+         // No need to add RnnPackage and MainReactPackage
+         return Arrays.<ReactPackage>asList(
+             // eg. new VectorIconsPackage()
+             new VectorIconsPackage()
+         );
+     }
+
+     @Override
+     public List<ReactPackage> createAdditionalReactPackages() {
+         return getPackages();
+     }
+
+     @Override
+      public String getJSMainModuleName() {
+        return "index.android";
 }
+ }
---
+++ shoplet/src/screens/Home/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {StyleSheet, View, Text} from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+    }
+})
+
+const Home = () => (<View style={styles.container}>
+    <Text style={styles.text}>Home Page</Text>
+    <Icon name="user" size={30} color="#900"/>
+</View>)
+
+export default Home
