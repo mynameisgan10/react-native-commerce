ea8dc9015f51a6db72e3cae88dba90d5607bbd31
Gan <geekwithgun@gmail.com>
2018-10-23 12:14:10 +0800

    added camera to ios
    using react-native-camera

--- shoplet/src/screens/Sell/Sell.js
+++ shoplet/src/screens/Sell/Sell.js
@@ -1,8 +1,36 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, TouchableHighlight} from 'react-native'
+import {CameraKitCamera} from 'react-native-camera-kit'
 
-const Sell = () => (<View>
-    <Text>Sell Page</Text>
-</View>)
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    takePhoto: {
+        position: 'absolute',
+        padding: 16,
+        bottom: 25,
+        alignItems: 'center',
+        right: 20,
+        left: 20
+        
+    }
+})
+
+
+const Sell = () => (
+    <View style={styles.container}>
+        <CameraKitCamera
+            ref={cam => this.camera = cam}
+            style={{
+                flex: 1,
+                backgroundColor: 'white'
+            }}
+            hideControls={false}/>
+            <TouchableHighlight style={styles.takePhoto}>
+                <Text>Take photo</Text>
+            </TouchableHighlight>
+    </View>
+)
 
 export default Sell
